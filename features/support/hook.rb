59b597ce0de92449b3eefbdac46f5e3e86a23ca2
rifkamalinda <rifkamalinda98@gmail.com>
2021-07-09 12:29:11 +0800

    comment changes

--- features/support/hook.rb
+++ features/support/hook.rb
@@ -1,6 +1,6 @@
 Before do |scenario|
     options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument("--window-size=1920,1080")
+    #options.add_argument("--window-size=1920,1080") 
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
